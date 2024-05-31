0d5e2f0bb2a0ae992eefe40ac660ca19ee654932
fumimelvindev <fumimelvindev@gmail.com>
2024-05-31 15:18:13 +0200

    Frontend: Products styling and display

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../Types';
+import { Product, Products } from '../../Types';
+import { ProductComponent } from '../product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  imports: [ProductComponent, CommonModule],
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
+
+  products: Product[] = [];
 
   ngOnInit() {
     this.productsService
       .getAllProducts('http://localhost:5000/api', {})
       .subscribe((products: Products) => {
         console.log(products.products);
+        this.products = products.products;
       });
   }
 }
