3edcee4c400ba8d7941825e4a0b6ab7180ce6093
fumimelvindev <fumimelvindev@gmail.com>
2024-05-31 21:04:55 +0200

    add pagination

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,6 +5,9 @@ const fs = require("fs");
 // @route GET /api
 // @access Public
 const getAllProducts = asyncHandler(async (req, res) => {
+  const page = parseInt(req.query.page) || 0;
+  const productsPerPage = parseInt(req.query.productsPerPage) || 10;
+
   fs.readFile("server/db.json", "utf8", (err, data) => {
     if (err) {
       console.log(err);
@@ -13,12 +16,17 @@ const getAllProducts = asyncHandler(async (req, res) => {
     }
 
     const jsonData = JSON.parse(data);
+    const start = page * productsPerPage;
+    const end = start + productsPerPage;
 
-    const result = jsonData.products.slice();
+    const result = jsonData.products.slice(start, end);
 
     res.status(200).json({
       products: result,
       total: jsonData.products.length,
+      page,
+      productsPerPage,
+      totalPages: Math.ceil(jsonData.products.length / productsPerPage),
     });
   });
 });
